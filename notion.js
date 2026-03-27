5ddc06cab5188171854adf922e031b16d1f5ce99
Hunain Shahid <hunainshahid032@gmail.com>
2026-03-27 20:04:35 +0500

    Add backspace logic on it

--- notion.js
+++ notion.js
@@ -1,3 +1,13 @@
+function setCursorToEnd(el) {
+    const range = document.createRange();
+    const selection = window.getSelection();
+    range.selectNodeContents(el);
+    range.collapse(false);
+
+    selection.removeAllRanges();
+    selection.addRange(range);
+}
+
 let editor = document.querySelector("#editor");
 
 editor.addEventListener("keydown", (e) => {
@@ -8,7 +18,20 @@ editor.addEventListener("keydown", (e) => {
         newdiv.className = "block";
         newdiv.contentEditable = "true";
 
-        e.target.after(newdiv);
+        e.target.closest(".block").after(newdiv);
         newdiv.focus();
+
+    } else if (e.key === "Backspace") {
+        let block = e.target.closest(".block");
+        let prevBlock = block.previousElementSibling;
+
+        if (block.innerText.trim().length == 0 && prevBlock) {
+            e.preventDefault();
+
+            block.remove();
+            prevBlock.focus();
+            setCursorToEnd(prevBlock);
+
+        }
     }
 });
